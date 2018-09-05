275c1711a7a241b4b330528a30a3fbfbd4973eb3
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2018-09-05 14:21:01 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="treetagger-xml",
-    version="0.1",
+    version="0.1.1",
     author="Martijn van der Klis",
     author_email="m.h.vanderklis@uu.nl",
     description="Reads .xml-files and parses these with TreeTagger",
@@ -13,10 +13,14 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/time-in-translation/treetagger-xml",
     packages=setuptools.find_packages(),
+    license="MIT",
     classifiers=[
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3",
+        "Topic :: Text Processing :: Linguistic",
+        "Programming Language :: Python :: 2.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+        'lxml', 'six', 'treetaggerwrapper'
+    ]
 )
