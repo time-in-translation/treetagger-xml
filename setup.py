07daa2711c29cd1e7f7758693a5727147dc9ab8e
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2018-09-07 10:06:27 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="treetagger-xml",
-    version="0.1.2",
+    version="0.1.3",
     author="Martijn van der Klis",
     author_email="m.h.vanderklis@uu.nl",
     description="Reads .xml-files and parses these with TreeTagger",
